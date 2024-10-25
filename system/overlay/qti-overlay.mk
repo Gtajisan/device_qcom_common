6daf66e1928c49b041074cdcfb59ca91027c59b9
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-10-25 22:00:32 +0600

    Ok fine I'm just remove it now

--- system/overlay/qti-overlay.mk
+++ system/overlay/qti-overlay.mk
@@ -15,13 +15,3 @@
 PRODUCT_SOONG_NAMESPACES += \
     device/qcom/common/system/overlay
 
-# Overlays
-PRODUCT_PACKAGES += \
-    CarrierConfigResCommon_Sys \
-    FrameworksResCommon_Sys \
-    SettingsResCommon_Sys \
-    SystemUIResCommon_Sys \
-    TelecommResCommon_Sys \
-    TelephonyResCommon_Sys \
-    WifiResCommon_Sys \
-    WifiResCommonMainline_Sys
